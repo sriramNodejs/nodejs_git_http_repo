7094932727c5c66ffef321d6d6db85ba299db5dc
sriramNodejs <dsriram50404@gmail.com>
2026-07-14 19:26:01 +0530

    commit message

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+  res.send('Hello World from post api');
+});
+
+app.put('/', (req, res) => {
+  res.send('Hello World from put api');
+});
+console.log('this is my new code from switch command');
+console.log('this is my new code');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
